b056328f05eec22fb7197b55194a21ca0f958f74
Veera Reddy Obula <veerareddy.obula@gmail.com>
2018-02-11 11:12:05 +0000

    login page responsive issue fixed

--- src/probudget/auth/LoginPage.js
+++ src/probudget/auth/LoginPage.js
@@ -45,31 +45,27 @@ class LoginPage extends React.Component{
       <div className="app flex-row align-items-center">
         <div className="container">
           <div className="row justify-content-center">
-            <div className="col-md-8">
-              <div className="card-group">
-                <div className="card p-4">
-                  <div className="card-body">
-                    <div className="d-flex flex-column">
-                    <h1 className="text-center">Pro-Budget</h1>
-                    <div className="about-pro-budget"></div>
-                    <div className="d-flex flex-column justify-content-end">
-                      <button type="button" className="btn btn-googleplus btn-lg" onClick={this.googleSignin}><i className="fa fa-google-plus fa-fw"></i> Google+</button>
-                    </div>
-                    <p className="text-muted">Sign In to your account via google</p>
-                    </div>
+            <div className="col-md-4" style={{padding:'0px'}}>
+              <div className="card card-body">
+                <div className="d-flex flex-column">
+                <h1 className="text-center">Pro-Budget</h1>
+                <div className="about-pro-budget"></div>
+                <div className="d-flex flex-column justify-content-end">
+                  <button type="button" className="btn btn-googleplus btn-lg" onClick={this.googleSignin}><i className="fa fa-google-plus fa-fw"></i> Google+</button>
+                </div>
+                <p className="text-muted">Sign In to your account via google</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4" style={{padding:'0px'}}>
+              <div className="card card-body text-center text-white bg-primary" style={{backgroundColor:'transparent', minHeight: '100%'}}>
+                <div>
+                  <p>Pro-Budget  personal finance software is the perfect solution for managing your finances. You can organize your cash flow, expenses and bank accounts in one place. And easy to understand charts and graphs enable you to visualize how you’re spending your money month-to-month. This enables you to track your progress, discover where you can make savings and improve how you manage your money.</p>
+                <div className="row pull-right">
+                  <div className="col-6">
+                    <button type="button" className="btn btn-warning"><i className="fa fa-info-circle"></i> More info!!</button>
                   </div>
                 </div>
-                <div className="card text-white bg-primary py-5 d-md-down-none" style={{width:"44%"}}>
-                  <div className="card-body text-center">
-                    <div>
-                      <p>Pro-Budget  personal finance software is the perfect solution for managing your finances. You can organize your cash flow, expenses and bank accounts in one place. And easy to understand charts and graphs enable you to visualize how you’re spending your money month-to-month. This enables you to track your progress, discover where you can make savings and improve how you manage your money.</p>
-                    <div className="row pull-right">
-                      <div className="col-6">
-                        <button type="button" className="btn btn-warning"><i className="fa fa-info-circle"></i> More info!!</button>
-                      </div>
-                    </div>
-                    </div>
-                  </div>
                 </div>
               </div>
             </div>
